f59facdc6d4cbc6cc3d7af4d046786910c1a47eb
vasu-chaturvedi <109227883+vasu-chaturvedi@users.noreply.github.com>
2025-07-15 00:08:14 +0530

    cleanup

--- vendor/github.com/VictoriaMetrics/easyproto/doc.go
+++
@@ -1,3 +0,0 @@
-// Package easyproto provides building blocks for marshaling and unmarshaling protobuf v3 messages
-// according to https://protobuf.dev/programming-guides/encoding/ .
-package easyproto
